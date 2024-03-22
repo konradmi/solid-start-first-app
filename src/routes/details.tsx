35c1168ffe71a34c8e27d222c752dab82d123289
Konrad Miekus <miekus.konrad@gmail.com>
2024-03-22 09:26:15 +0100

    try caching

--- src/routes/details.tsx
+++ src/routes/details.tsx
@@ -1,23 +1,24 @@
-import { createSignal, createResource } from "solid-js"
+import { createSignal } from "solid-js"
+import { cache, createAsync } from "@solidjs/router"
 
-const getPost = async () => {
+const getPost = cache(async () => {
   console.log('fetching post')
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
   return (await response.json()).body
-}
+}, 'post')
  
 export const route = {
   load: () => getPost()
 };
 
 const Details = (props) => {
-  const [blog] = createResource(() => getPost())
+  const text = createAsync(() => getPost())
   const [count, setCount] = createSignal(0);
   return (
     <div>
       <h1>Details</h1>
       <h2>Count: {count()}</h2>
-      <div>{blog() && JSON.stringify(blog())}</div>
+      <div>{text() && JSON.stringify(text())}</div>
       <button onClick={() => setCount(count() + 10)}>Increment</button>
     </div>
   );
